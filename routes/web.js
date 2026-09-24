cfaf68a1f947a4fd20519cd88d44fbe55026c812
pranket1721 <prajuksagar17@gmail.com>
2026-09-24 18:47:23 +0530

    first commit

---
+++ routes/web.js
@@ -0,0 +1,73 @@
+var express=require('express');
+var router=express.Router();
+var mysql=require('mysql2');
+var util=require('util');
+router.use(express.urlencoded({extended:true}));
+
+router.use(express.urlencoded({extended:true}));
+var conn=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'simone'
+})
+var exe=util.promisify(conn.query).bind(conn);
+
+router.use(express.static('public'));
+router.get('/',async(req,res)=>{
+    var sql='select * from home ';
+    var data=await exe(sql)
+    res.render('web/index.ejs',{data:data[0]});
+})
+router.get('/about',async(req,res)=>{
+    var sql='select * from about';
+    var data=await exe (sql);
+    res.render('web/about.ejs',{data:data[0]});
+})
+router.get('/header',(req,res)=>{
+    res.render('web/header.ejs');
+})
+router.get('/footer',(req,res)=>{
+    res.render('web/footer.ejs');
+})
+router.get('/services',async(req,res)=>{
+    var sql='select * from service';
+    var data=await exe(sql);
+    res.render('web/services.ejs',{data:data});
+})
+router.get('/resume',async(req,res)=>{
+    var sql='select * from education';
+    var data=await exe(sql);
+    // res.render('web/resume.ejs',{education:data});
+       var sql2='select * from experience';
+    var experience=await exe(sql2);
+
+    var sql3='select * from skill';
+    var skill=await exe(sql3);
+    res.render('web/resume.ejs',{education:data,experience:experience,skill:skill});
+})
+router.get('/portfolio',async(req,res)=>{
+    var sql='select * from work';
+    var work=await exe(sql);
+    res.render('web/portfolio.ejs',{work:work});
+})
+router.get('/clients',async(req,res)=>{
+    var sql='select * from client';
+    var client=await exe(sql);
+    res.render('web/clients.ejs',{client:client});
+})
+router.get('/contact',(req,res)=>{
+    res.render('web/contact.ejs');
+})
+router.post('/contact_save',async(req,res)=>{
+    // res.send('contact_save');
+    // res.send(req.body);
+    var{name,email,message}=req.body;
+    var da=new Date();
+    var date1=da.getDate()+"-"+Number(da.getMonth()+1)+"-"+da.getFullYear();
+    // res.send(date1)
+    var sql='insert into contact_data(name,email,message,status,cdate)values(?,?,?,?,?)';
+    var data=await exe (sql,[name,email,message,'pending',date1]);
+    res.redirect('/contact');
+})
+module.exports=router;
---
+++ server.js
@@ -0,0 +1,7 @@
+var express=require('express');
+var app=express();
+var web=require('./routes/web.js');
+var admin=require('./routes/admin.js');
+app.use('/',web);
+app.use('/admin',admin);
+app.listen(3000);
